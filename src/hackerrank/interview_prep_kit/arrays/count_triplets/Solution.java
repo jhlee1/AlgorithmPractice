b3c2cd95d969b261c3d5fccc25a8e70e1d95fa26
joolee <joolee@ea.com>
2019-02-18 18:06:21 +0900

    Added a solution for count triplets ... still need to solve the rest of cases

---
+++ src/hackerrank/interview_prep_kit/arrays/count_triplets/Solution.java
@@ -0,0 +1,67 @@
+package hackerrank.interview_prep_kit.arrays.count_triplets;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the countTriplets function below.
+    static long countTriplets(List<Long> arr, long r) {
+        long numOfTriplets = 0;
+        Map<Long, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < arr.size(); i++) {
+            final long num = arr.get(i);
+
+            map.putIfAbsent(num, new ArrayList<>());
+            map.get(num).add(i);
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            final long currentNum = arr.get(i);
+            final long secondNum = currentNum * r;
+            final long thirdNum = currentNum * r * r;
+
+            if(map.containsKey(secondNum) && map.containsKey(thirdNum)) {
+                final int currentNumIndex = i;
+                long secondNumsIndexes = map.get(secondNum).stream()
+                        .filter(index -> index > currentNumIndex)
+                        .count();
+
+                long thirdNumIndexes = map.get(thirdNum).stream()
+                        .filter(index -> index > currentNumIndex)
+                        .count();
+
+                numOfTriplets += secondNumsIndexes * thirdNumIndexes;
+            }
+        }
+
+        return numOfTriplets;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nr[0]);
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long ans = countTriplets(arr, r);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
