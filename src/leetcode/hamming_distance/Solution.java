828f12d55bb4fb7a564fd1d01ba718586c240e87
jhlee1 <joohan33@gmail.com>
2019-05-13 16:05:39 +0900

    - Added a solution for hamming distance in leetcode

--- src/leetcode/hamming_distance/Solution.java
+++ src/leetcode/hamming_distance/Solution.java
@@ -1,4 +1,39 @@
 package leetcode.hamming_distance;
 
 public class Solution {
+    public int hammingDistance(int x, int y) {
+        int result = 0;
+        char[] xBinary = Integer.toBinaryString(x).toCharArray();
+        char[] yBinary = Integer.toBinaryString(y).toCharArray();
+        int shift = xBinary.length - yBinary.length;
+
+        if (shift > 0) {
+            for (int i = 0; i < shift; i++) {
+                if (xBinary[i] == '1') {
+                    result++;
+                }
+            }
+
+            for (int i = 0; i < yBinary.length; i++) {
+                if (yBinary[i] != xBinary[i + shift]) {
+                    result++;
+                }
+            }
+        } else {
+            shift = -shift;
+            for (int i = 0; i < shift; i++) {
+                if (yBinary[i] == '1') {
+                    result++;
+                }
+            }
+
+            for (int i = 0; i < xBinary.length; i++) {
+                if (xBinary[i] != yBinary[i + shift]) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
 }
